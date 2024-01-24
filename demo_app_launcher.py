30bdc3b3d0496c11fcb8458c566df45e4e870262
Leonid Zaburunov <zaburunovlv@student.bmstu.ru>
2024-01-24 10:40:58 +0300

    Установка 80 порта на прослушивание

--- demo_app_launcher.py
+++ demo_app_launcher.py
@@ -2,5 +2,5 @@ import sys
 import streamlit.web.cli as stcli
 
 if __name__ == '__main__':
-    sys.argv = ["streamlit", "run", "demo_app/demo_app.py", "--server.enableXsrfProtection=false"]
+    sys.argv = ["streamlit", "run", "demo_app/demo_app.py", "--server.port=80", "--server.enableXsrfProtection=false"]
     sys.exit(stcli.main())
